d9f97adfad3ae01fdbd43a5dc7657de063a92857
Van Liem <vanliem2204@gmail.com>
2017-09-14 17:14:45 +0700

    17

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,8 +12,13 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <style>
+        body { padding-bottom: 100x;}
+        .level {  display: flex, align-items: center; }
+        .flex { flex: 1; }
+    </style>
 </head>
-<body style="padding-bottom: 100px;">
+<body>
     <div id="app">
         <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
