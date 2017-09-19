a18deac66b5788bf7b112358cee8d3601a4e5c27
Van Liem <vanliem2204@gmail.com>
2017-09-19 11:05:33 +0700

    tmp_for_24

---
+++ app/Activity.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Activity extends Model
+{
+    protected $guarded = [];
+
+    public function subject()
+    {
+    	return $this->morphTo();
+    }
+}
--- app/Reply.php
+++ app/Reply.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    use FavouriteTable;
+    use FavouriteTable, RecordsActivity;
     
 	protected $guarded = [];
 	protected $with = ['owner', 'favourites'];
