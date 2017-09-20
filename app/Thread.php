99495efd2ea8ed7a8cfaafe19f6c52c46138036f
Van Liem <vanliem2204@gmail.com>
2017-09-20 09:14:28 +0700

    Fix bug delete thread

--- app/Thread.php
+++ app/Thread.php
@@ -19,7 +19,7 @@ class Thread extends Model
         static::addGlobalScope(new ReplyCountScope);
 
         static::deleting(function ($thread) {
-            $thread->replies()->delete();
+            $thread->replies->each->delete();
         });
     }
 
