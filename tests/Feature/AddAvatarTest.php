1ec793bb12dd5067272b45416781e1581040235b
Van Liem <vanliem2204@gmail.com>
2017-10-04 10:00:51 +0700

    Testing Avatar Uploads: Part 1

---
+++ tests/Feature/AddAvatarTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AddAvatarTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+    */
+    public function only_members_can_add_avatars()
+    {
+        $user = create('App\User');
+
+        $this->withExceptionHandling();
+
+        $this->json('POST', "api/users/{$user}/avatar", [])
+            ->assertStatus(401);
+    }
+
+    /**
+     * @test
+    */
+    public function a_valid_avatar_must_be_provided()
+    {
+        $this->withExceptionHandling()
+            ->signedIn();
+
+        $this->json('POST', "api/users/". auth()->id() ."/avatar", [
+            'avatar' => 'not-an-image'
+        ])
+        ->assertStatus(422);
+    }
+
+    /**
+     * @test
+    */
+    public function a_user_may_add_an_avatar_to_their_profile()
+    {
+        $this->signedIn();
+
+        Storage::fake('public');
+
+        $this->json('POST', 'api/users/'. auth()->id() .'/avatar', [
+            'avatar' => $file = UploadedFile::fake()->image('avatar.jpg')
+        ]);
+
+        $this->assertEquals("avatars/{$file->hashName()}", auth()->user()->avatar_path);
+
+        Storage::disk('public')
+            ->assertExists('avatars/' . $file->hashName());
+    }
+}
