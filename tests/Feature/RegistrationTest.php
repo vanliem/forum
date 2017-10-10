9f6704a8662521847b7c16de4e356937bd6c4777
Van Liem <vanliem2204@gmail.com>
2017-10-10 09:39:35 +0700

    Email Confirmation Loose Ends

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -49,7 +49,10 @@ class RegistrationTest extends TestCase
         $this->get(route('register.confirm', ['token' => $user->confirmation_token]))
             ->assertRedirect(route('threads.index'));
 
-        $this->assertTrue($user->fresh()->confirmed);
+        tap($user->fresh(), function ($user) {
+            $this->assertTrue($user->confirmed);
+            $this->assertNull($user->confirmation_token);
+        });
     }
 
 
